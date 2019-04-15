ad3b7173cbeb9eae4e3894815100485995d686e5
James Woo <jamesjaywoo@gmail.com>
2019-04-15 19:08:13 -0400

    Fix head

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Helmet } from "react-helmet";
 import Main from "./components/Main";
 import Login from "./components/Login";
 import NotFound from "./components/NotFound";
@@ -11,10 +10,6 @@ class App extends Component {
   render() {
     return (
       <div className="application">
-        <Helmet>
-          <title>James Woo</title>
-          <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
-        </Helmet>
         <Router>
           <Switch>
             <Route path="/" exact component={Main} />
